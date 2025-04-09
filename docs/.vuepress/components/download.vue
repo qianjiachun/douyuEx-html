4e2a62cd4693591fb4cea4b5672aff1ef8f3f987
qianjiachun <189964430@qq.com>
2025-04-09 15:20:20 +0800

    init

--- docs/.vuepress/components/download.vue
+++ docs/.vuepress/components/download.vue
@@ -7,7 +7,7 @@ export default {
     data() {
         //这里存放数据
         return {
-            currentVersion: "2025.03.25.01"
+            currentVersion: "2025.04.09.01"
         };
     },
     methods: {
