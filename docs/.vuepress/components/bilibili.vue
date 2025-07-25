ea6c871dfd1bc65e05244469837c9cadae9cb014
qianjiachun <189964430@qq.com>
2025-07-25 15:55:31 +0800

    init

--- docs/.vuepress/components/bilibili.vue
+++ docs/.vuepress/components/bilibili.vue
@@ -1,6 +1,7 @@
 <template>
     <div class='bilibili'>
-        <iframe class="frame" src="//player.bilibili.com/player.html?aid=502020627&bvid=BV1HN411Q7ZP&cid=310116472&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+        <!-- <iframe class="frame" src="//player.bilibili.com/player.html?aid=502020627&bvid=BV1HN411Q7ZP&cid=310116472&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe> -->
+        视频安装教程：<a href="https://www.bilibili.com/video/BV1HN411Q7ZP" target="_blank">https://www.bilibili.com/video/BV1HN411Q7ZP</a>
     </div>
 </template>
 
