aa22cfdead27bfd0122c8d1260d24bd9031bb9ed
qianjiachun <189964430@qq.com>
2025-12-01 15:50:27 +0800

    init

--- docs/.vuepress/components/download.vue
+++ docs/.vuepress/components/download.vue
@@ -7,7 +7,7 @@ export default {
     data() {
         //这里存放数据
         return {
-            currentVersion: "2025.12.01.01"
+            currentVersion: "2025.12.01.02"
         };
     },
     methods: {
